5764d0f0ed1de7d3d90c8877fb9595a8056b00d7
GabyGaby412 <g.velazquez2@ufl.edu>
2025-11-28 12:38:08 -0500

    added related note function, just need to call

--- backend/app.py
+++ backend/app.py
@@ -197,7 +197,8 @@ def get_advice():
         return jsonify({"error": "missing note text"}), 400
     advice = give_advice(note_text)
     return jsonify({"advice": advice})
-def get_notes_raw():
+
+def get_actual_notes():
     try:
         res = supabase.table("notes").select("*").execute()
         return res.data
